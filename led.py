ee5a29b7c846fa52addf32a09cd4f9221e054e52
menon-sanjay <34072312+menon-sanjay@users.noreply.github.com>
2024-01-30 13:39:56 -0800

    Create led.py

---
+++ led.py
@@ -0,0 +1,53 @@
+"""! @file led.py
+This program controls the brightness of an LED using a PWM signal
+sent over pin PAO.
+"""
+
+import utime
+import pyb
+
+# def timer_int (tim_num):
+#     
+#     
+#     if (dty >= 100) :
+#         dty = 0
+#     dty = 100 - dty
+#     led_brightness(dty)
+    
+
+def led_setup ():
+    """! Doxygen style docstring for this function """
+    global ch1
+    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+    tim2 = pyb.Timer(2, freq=20000)
+    ch1 = tim2.channel(1, pyb.Timer.PWM_INVERTED, pin=pinA0)
+    
+    timmy = pyb.Timer(1, freq = 20)
+    timmy.counter ()
+    timmy.callback(led_brightness)
+    
+    global dty
+    dty = 100
+    return ch1
+
+def led_brightness (tim_num):
+    """! Doxygen style docstring for this function """
+    global ch1
+    global dty
+    
+    if (dty == 0 ):
+        dty = 100
+    dty -= 1
+    
+    ch1.pulse_width_percent(dty)
+
+if __name__ == "__main__":
+    try:
+        ch1 = led_setup()
+        
+        while True:
+            continue
+
+    except(KeyboardInterrupt):
+        print("Program Terminated")
+        
